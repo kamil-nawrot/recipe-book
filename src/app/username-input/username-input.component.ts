d43a3536a30088d68433eba8757f851d6224ad37
Kamil Nawrot <kamil.r.nawrot@comarch.com>
2021-10-21 15:53:42 +0200

    moved checking if username is empty to template expression

--- src/app/username-input/username-input.component.ts
+++ src/app/username-input/username-input.component.ts
@@ -8,16 +8,9 @@ import { Component, OnInit } from '@angular/core';
 export class UsernameInputComponent implements OnInit {
 
   username = ""
-  isUsernameEmpty = true
-
-  onUsernameChange(event: Event) {
-    let value = (<HTMLInputElement>event.target).value
-    value.length === 0 ? this.isUsernameEmpty = true : this.isUsernameEmpty = false
-  }
 
   onReset() {
     this.username = ""
-    this.isUsernameEmpty = true
   }
 
   constructor() { }

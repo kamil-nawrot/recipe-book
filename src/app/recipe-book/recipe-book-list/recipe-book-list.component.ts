e990ba204ac5b771c5dc81460a691b9a31609a34
Kamil Nawrot <kamil.r.nawrot@comarch.com>
2021-10-22 14:53:57 +0200

    added Recipe model, created recipe list and scaffolded recipe details component

--- src/app/recipe-book/recipe-book-list/recipe-book-list.component.ts
+++ src/app/recipe-book/recipe-book-list/recipe-book-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from "../recipe.model";
 
 @Component({
   selector: 'app-recipe-book-list',
@@ -6,6 +7,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-book-list.component.css']
 })
 export class RecipeBookListComponent implements OnInit {
+
+  public recipes: Recipe[] = [
+    new Recipe("Test Recipe", "This is simply a test", "https://loveincorporated.blob.core.windows.net/contentimages/gallery/d9e900e4-212e-4c3d-96d5-cb14a023c659-worlds-most-delicious-dishes.jpg"),
+    new Recipe("Second Recipe", "This is simply a test", "https://loveincorporated.blob.core.windows.net/contentimages/gallery/d9e900e4-212e-4c3d-96d5-cb14a023c659-worlds-most-delicious-dishes.jpg")
+
+  ]
 
   constructor() { }
 

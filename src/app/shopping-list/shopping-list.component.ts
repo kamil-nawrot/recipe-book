11338406a81d71e1732f250f5e3d62e2a02858ab
Kamil Nawrot <kamil.r.nawrot@comarch.com>
2021-10-22 16:26:41 +0200

    created Ingredient model, added ingredient list inside shopping list

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Ingredient } from "../shared/models/ingredient.model";
 
 @Component({
   selector: 'app-shopping-list',
@@ -6,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent implements OnInit {
+
+  public ingredients: Ingredient[] = [
+    new Ingredient("flour", 200, "g"),
+    new Ingredient("eggs", 4, "pcs")
+  ]
 
   constructor() { }
 

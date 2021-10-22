a06170bd89e9edc3b147dd5a737c0048c2c0e020
Kamil Nawrot <kamil.r.nawrot@comarch.com>
2021-10-22 11:00:01 +0200

    restructured directories, move all training components to playground/ folderq

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { ServerComponent } from './server/server.component';
-import { WarningAlertComponent } from './warning-alert/warning-alert.component';
-import { SuccessAlertComponent } from './success-alert/success-alert.component';
-import { ServerListComponent } from './server-list/server-list.component';
-import { UsernameInputComponent } from './username-input/username-input.component';
-import { PasswordToggleComponent } from './password-toggle/password-toggle.component';
+import { ServerComponent } from './playground/server/server.component';
+import { WarningAlertComponent } from './playground/warning-alert/warning-alert.component';
+import { SuccessAlertComponent } from './playground/success-alert/success-alert.component';
+import { ServerListComponent } from './playground/server-list/server-list.component';
+import { UsernameInputComponent } from './playground/username-input/username-input.component';
+import { PasswordToggleComponent } from './playground/password-toggle/password-toggle.component';
 
 @NgModule({
   declarations: [

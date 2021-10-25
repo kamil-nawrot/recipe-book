3993c3c3800e8d31d6849b2fd61d61c85a42e4fe
Kamil Nawrot <kamil.r.nawrot@comarch.com>
2021-10-25 12:18:33 +0200

    implemented selecting recipe details by passing data using event emitters

--- src/app/recipe-book/recipe-book-list/recipe-book-list.component.ts
+++ src/app/recipe-book/recipe-book-list/recipe-book-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, EventEmitter, OnInit, Output} from '@angular/core';
 import { Recipe } from "../recipe.model";
 
 @Component({
@@ -8,15 +8,20 @@ import { Recipe } from "../recipe.model";
 })
 export class RecipeBookListComponent implements OnInit {
 
+  @Output() selectRecipe = new EventEmitter<Recipe>()
+
   public recipes: Recipe[] = [
     new Recipe("Test Recipe", "This is simply a test", "https://loveincorporated.blob.core.windows.net/contentimages/gallery/d9e900e4-212e-4c3d-96d5-cb14a023c659-worlds-most-delicious-dishes.jpg"),
-    new Recipe("Second Recipe", "This is simply a test", "https://loveincorporated.blob.core.windows.net/contentimages/gallery/d9e900e4-212e-4c3d-96d5-cb14a023c659-worlds-most-delicious-dishes.jpg")
-
+    new Recipe("Secondddd Recipe", "This is simply a test 222222", "https://loveincorporated.blob.core.windows.net/contentimages/gallery/d9e900e4-212e-4c3d-96d5-cb14a023c659-worlds-most-delicious-dishes.jpg")
   ]
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onRecipeSelected(recipe: Recipe) {
+    this.selectRecipe.emit(recipe)
   }
 
 }

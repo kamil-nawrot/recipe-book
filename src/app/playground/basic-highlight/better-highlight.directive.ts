5d3739baf7e3e8fd60364ef571c840afe23fc4ec
Kamil Nawrot <kamil.r.nawrot@comarch.com>
2021-10-25 14:52:10 +0200

    created highlight directive with HostListener decorator

--- src/app/playground/basic-highlight/better-highlight.directive.ts
+++ src/app/playground/basic-highlight/better-highlight.directive.ts
@@ -1,4 +1,4 @@
-import {Directive, ElementRef, OnInit, Renderer2} from '@angular/core';
+import {Directive, ElementRef, HostListener, OnInit, Renderer2} from '@angular/core';
 
 @Directive({
   selector: '[appBetterHighlight]'
@@ -8,10 +8,23 @@ export class BetterHighlightDirective implements OnInit {
   constructor(private elementRef: ElementRef, private renderer: Renderer2) { }
 
   ngOnInit() {
+
+  }
+
+  @HostListener("mouseenter") mouseIn(event: Event) {
     this.renderer.setStyle(
       this.elementRef.nativeElement,
       "background-color",
-      "green")
+      "green"
+    )
+  }
+
+  @HostListener("mouseleave") mouseOut(event: Event) {
+    this.renderer.setStyle(
+      this.elementRef.nativeElement,
+      "background-color",
+      "inherit"
+    )
   }
 
 }

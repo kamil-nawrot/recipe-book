30122ad7a98dfacc6614fca7ec77d8ff56bba6c6
Kamil Nawrot <kamil.r.nawrot@comarch.com>
2021-11-03 11:22:55 +0100

    displayed results in the template upon submission, finished solution

--- src/app/playground/forms/template-driven-form-assignment/template-driven-form-assignment.component.ts
+++ src/app/playground/forms/template-driven-form-assignment/template-driven-form-assignment.component.ts
@@ -9,6 +9,13 @@ import {NgForm} from "@angular/forms";
 export class TemplateDrivenFormAssignmentComponent implements OnInit {
 
   @ViewChild("form") formData: NgForm | undefined
+  defaultType = "advanced"
+  isSubmitted = false
+  results = {
+    email: "",
+    password: "",
+    type: ""
+  }
 
   constructor() { }
 
@@ -18,6 +25,11 @@ export class TemplateDrivenFormAssignmentComponent implements OnInit {
 
   onSubmit() {
     console.log(this.formData)
+    this.results.email = this.formData?.value.credentials.username
+    this.results.password = this.formData?.value.credentials.password
+    this.results.type = this.formData?.value.type
+    this.isSubmitted = true
+    this.formData?.resetForm()
   }
 
 }

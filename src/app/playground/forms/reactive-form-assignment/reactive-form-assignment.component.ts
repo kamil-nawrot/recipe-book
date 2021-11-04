3ad9e6cabc7dcd3afa111de3a97d8d2130dcf1a5
Kamil Nawrot <kamil.r.nawrot@comarch.com>
2021-11-04 09:28:30 +0100

    created validators

--- src/app/playground/forms/reactive-form-assignment/reactive-form-assignment.component.ts
+++ src/app/playground/forms/reactive-form-assignment/reactive-form-assignment.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {FormControl, FormGroup, Validators} from "@angular/forms";
+import {Observable} from "rxjs";
 
 @Component({
   selector: 'app-reactive-form-assignment',
@@ -7,9 +9,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ReactiveFormAssignmentComponent implements OnInit {
 
-  constructor() { }
+  form: FormGroup
+  forbiddenNames = ["test"]
+
+  constructor() {
+    this.form = new FormGroup({
+      "name": new FormControl(null, Validators.required, this.allowedNames),
+      "email": new FormControl(null, [Validators.required, Validators.email]),
+      "status": new FormControl("stable")
+    })
+  }
 
   ngOnInit(): void {
   }
+
+  allowedNames = (control: FormControl): Observable<{[s: string]: boolean} | null> => {
+    return new Observable<{[s: string]: boolean} | null>(observer => {
+      setTimeout(() => {
+        if (this.forbiddenNames.includes(control.value.toString().toLowerCase())) {
+          observer.next({"nameIsForbidden": true})
+        } else observer.next(null)
+        observer.complete()
+      }, 1000)
+    })
+  }
+
+
 
 }

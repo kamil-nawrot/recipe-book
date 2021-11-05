2e0e6dccdead3fec968cbc762292103bdbc55837
Kamil Nawrot <kamil.r.nawrot@comarch.com>
2021-11-05 12:06:20 +0100

    created and implemented custom sort pipe for sorting array of servers

--- src/app/playground/pipes/sort.pipe.ts
+++ src/app/playground/pipes/sort.pipe.ts
@@ -1,12 +1,13 @@
 import { Pipe, PipeTransform } from '@angular/core';
 
 @Pipe({
-  name: 'sort'
+  name: 'sort',
+  pure: false
 })
 export class SortPipe implements PipeTransform {
 
-  transform(value: unknown, ...args: unknown[]): unknown {
-    return null;
+  transform(value: any): any {
+    return value.sort((a, b) => a.name > b.name ? 1 : -1)
   }
 
 }
